988b48f25af87ba840e7e0001def6914bee65314
Rafael Dalprá <thedalpra@gmail.com>
2017-05-21 23:11:49 +0100

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -46,6 +46,8 @@ namespace :puma do
   before :start, :make_dirs
 end
 
+Rake::Task['deploy:migrate'].clear_actions
+
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
@@ -54,6 +56,16 @@ namespace :deploy do
         puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
         exit
+      end
+    end
+  end
+
+  desc 'Migration'
+  task :migrate do
+    on roles(:app) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+        end
       end
     end
   end
